cbee8b32f4201aa860637c9385c70cd925b5a1a1
kook16 <jamesss075@gmail.com>
2023-11-24 19:03:55 +0300

    fixed requirement check on task 1

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
-exec {'kill-killmenow':
-    command => 'pkill killmenow',
-    path    => '/usr/bin';
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
