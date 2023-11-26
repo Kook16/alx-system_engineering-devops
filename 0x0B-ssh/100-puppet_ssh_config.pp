7e389d9574f9bb16de7fb0949a91a7e3b1b4e8d4
kook16 <jamesss075@gmail.com>
2023-11-26 21:43:55 +0300

    updated advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,14 @@ include stdlib
 
 file_line { 'use the private key ~/.ssh/school':
     ensure  => present,
-    line    => '    Identityfile ~/.ss/ssh/school',
+    line    => '    Identityfile ~/ssh/school',
     path    => '/etc/ssh/ssh_config',
     replace => true,
 }
 
 file_line { 'authenicate without password':
     ensure  => present,
-    line    => '    PasswordAuthenication no',
+    line    => '    PasswordAuthentication no',
     path    => '/etc/ssh/ssh_config',
     replace => true,
 }
