a43ca066b75c1785f95efaae85fe9cd51082eafc
kook16 <jamesss075@gmail.com>
2023-12-05 03:47:00 +0300

    updates

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,7 @@
 # automate the task of creating a custom HTTP header response using puppet
 
 exec { 'apt-update':
-    command => 'usr/bin/apt-get update'
+    command => '/usr/bin/apt-get update'
 }
 
 package { 'NGINX':
@@ -11,10 +11,11 @@ package { 'NGINX':
 
 file_line { 'add line to the nginx conf file':
     path    => '/etc/nginx/nginx.conf',
-    line    => '\tadd_header X-Served-By ${hostname};",
+    line    => '\tadd_header X-Served-By ${hostname};',
     after   => 'http {',
 }
 
 exec { 'restart nginx':
     command => 'usr/sbin/service nginx restart'
+    require => package['nginx'],
 }
