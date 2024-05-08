140e32935735089ca7b65b5a67eda16c4c0c855f
Kok <jamesss075@gmail.com>
2024-05-08 04:34:00 +0300

    fixed pycodesytle

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,24 +3,29 @@
 import requests
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"]
-        for post in posts:
-            print(post["data"]["title"])
-    else:
+    '''a function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.'''
+    if subreddit is None or type(subreddit) is not str:
         print(None)
 
-# Test the function
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        top_ten(subreddit)
+    headers = {'User-Agent': 'VUTHhzWkxrTG56djBsR3dhN'}
+    parameters = {'limit': 10}
+
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+
+    response = requests.get(url, headers=headers, params=parameters,
+                            allow_redirects=False)
+    try:
+        if response.status_code >= 400:
+            print(None)
+            return
+
+        data = response.json()
+        if data is None:
+            print(None)
+
+        posts = data.get('data').get('children')
+
+        [print(post.get('data').get('title')) for post in posts]
+    except Exception:
+        print(None)
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,22 +2,28 @@
 '''API MODULE'''
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
     if after is None:
         url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
     else:
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={after}"
-    
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
-    
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json" \
+          f"?limit=100&after={after}"
+
+    headers = {
+        "User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                       "AppleWebKit/537.36 (KHTML, like Gecko) "
+                       "Chrome/58.0.3029.110 Safari/537.3")
+    }
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         data = response.json()
         posts = data["data"]["children"]
         for post in posts:
             hot_list.append(post["data"]["title"])
-        
+
         after = data["data"]["after"]
         if after:
             return recurse(subreddit, hot_list, after)
@@ -25,6 +31,7 @@ def recurse(subreddit, hot_list=[], after=None):
             return hot_list
     else:
         return None
+
 
 # Test the function
 if __name__ == "__main__":
